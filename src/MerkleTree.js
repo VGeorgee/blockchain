fe188ffab10311c41a1118f5219471d0ae96331b
Vgeorgee <cracken1111@gmail.com>
2020-10-11 12:44:43 +0200

    feat: basic merkle tree functions

---
+++ src/MerkleTree.js
@@ -0,0 +1,52 @@
+let sha = require('js-sha256').sha256;
+let sha256 = (data) => {
+    return sha(sha(typeof data === 'string' ? data : JSON.stringify(data)))
+}
+
+module.exports = {
+    buildTree: function (data) {
+        if(data.length % 2 != 0){
+            data.push(data[data.length - 1])
+        }
+        let leafNodes = []
+
+        data.forEach(element => {
+            leafNodes.push(this.createLeafNode(element))
+        })
+
+        let nodes = leafNodes
+        while(nodes.length != 1){
+            if(nodes.length % 2 != 0){
+                nodes.push(nodes[nodes.length - 1])
+            }
+
+            let nodes2 = []
+            for(let i = 0; i < nodes.length; i += 2) {
+                nodes2.push(this.createNode(nodes[i], nodes[i + 1]))
+            }
+
+            nodes = nodes2
+        }
+        return nodes[0]
+    },
+
+    createLeafNode(transaction){
+        return {
+            hash: sha256(transaction),
+            transaction
+        }
+    },
+
+    isLeafNode(node){
+        return !!node.transaction
+    },
+
+    createNode(leftNode, rightNode){
+        return {
+            hash: sha256(leftNode.hash + rightNode.hash),
+            left: leftNode,
+            right: rightNode || leftNode
+        }
+    },
+    
+}
