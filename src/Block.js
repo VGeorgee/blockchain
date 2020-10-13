33bdd6a99a3faa4ac46103ce2afc3bfdaa7a904c
Vgeorgee <cracken1111@gmail.com>
2020-10-13 23:20:23 +0200

    feat: add block class

---
+++ src/Block.js
@@ -0,0 +1,52 @@
+let {sha256, convertHexToBin} = require('./utils') 
+let MerkleTree = require('./MerkleTree')
+
+
+function isValidProof(proof, difficulty){
+    proof = convertHexToBin(proof)
+    for(let i = 0; i < difficulty; i++){
+        if(proof[i] != '0'){
+            return false
+        }
+    }
+    return true
+}
+
+module.exports = class Block {
+    constructor() {
+        this.header = {
+            timestamp: Date.now(),
+            nonce: 0,
+            merkleRoot: 0,
+            difficulty: 0
+        }
+        this.merkleTree = null
+        this.headerHash = null
+    }
+    calculateTransactions(transactions) {
+        this.merkleTree = MerkleTree.buildTree(transactions)
+        this.header.merkleRoot = this.merkleTree.hash
+    }
+    _createHeaderHash() {
+        this.headerHash = sha256(this.header)
+    }
+    _calculateProofOfWork(diff) {
+        if(!this.headerHash){
+            this._createHeaderHash()
+        }
+        while(!isValidProof(this.headerHash, diff || this.header.difficulty)){
+            this.header.nonce++
+            this._createHeaderHash()
+        }
+    }
+    getHash(){
+        if(!this.headerHash){
+            this._calculateProofOfWork()
+        }
+        return this.headerHash
+    }
+    verifyTransaction(transaction){
+        return MerkleTree.verifyTransaction(this.merkleTree, transaction)
+    }
+}
+
