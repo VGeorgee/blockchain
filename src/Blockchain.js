20eb9a1894603fe6549a486bd6e10aac0d866365
Vgeorgee <cracken1111@gmail.com>
2020-11-29 19:32:10 +0100

    refactor: validation of transactions

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -1,4 +1,11 @@
 let {Block, createGenesis} = require('./Block')
+const crypto = require('crypto')
+
+function verifySignature(transaction){
+    let verifier = crypto.createVerify('sha256').update(transaction.message).end()
+    return verifier.verify(transaction.publicKey, Buffer.from(transaction.signature, 'hex'))
+}
+
 
 module.exports = class Blockchain {
     #blocks
@@ -7,6 +14,8 @@ module.exports = class Blockchain {
     }
 
     confirm(transactions) {
+        transactions = this.validateAllIncomingTransactions(transactions)
+        
         let block = new Block(transactions, this.#blocks.length)
         block.setPreviousHash(this.getCurrentBlock().getHash())
         this.#blocks.push(block)
@@ -26,6 +35,44 @@ module.exports = class Blockchain {
 
     verifyTransactionInBlock(index, transaction){
         return this.#blocks[index].verifyTransaction(transaction)
+    }
+
+    
+    validateAllIncomingTransactions(transactions){
+        for (let i = 0; i < transactions.length; i++){
+
+            const transaction = transactions[i]
+            
+            if(!verifySignature(transaction)){
+                console.log("Transaction deleted, invalid signature");
+                delete transactions[i]
+                continue
+            }
+            
+            let balanceOfWallet = this.getBalanceOfWallet(transaction.from)
+
+            if(balanceOfWallet < transaction.volume){
+                console.log("Transaction deleted, invalid balance");
+                delete transactions[i]
+            }
+            console.log(`Transaction approved from ${transaction.from} to ${transaction.to} with volume ${transaction.volume}`);
+            transaction.balance = balanceOfWallet
+        }
+
+        return transactions.filter(e => e)
+    }
+
+    getBalanceOfWallet(wallet){
+        let balance = 0
+        let index = this.getBlockHeight() - 1
+        let block
+        do {
+            block = this.#blocks[index--]
+            if(block.transactionsOut[wallet]){
+                balance += block.getSumOfTransactionsToWallet(wallet)
+            }
+        } while (index >= 0 && !block.transactionsIn[wallet])
+        return balance
     }
 
 }
